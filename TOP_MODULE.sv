b4191341748db139d8b6064167b0964548b4f2b3
Rigo17 <rigo.venegas17@gmail.com>
2017-12-06 23:11:09 -0300

    2
    funciona el i2c, falta transmison por uart

--- TOP_MODULE.sv
+++ TOP_MODULE.sv
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 04.12.2017 20:46:55
+// Create Date: 06.12.2017 18:17:13
 // Design Name: 
 // Module Name: TOP_MODULE
 // Project Name: 
@@ -27,24 +27,21 @@ module TOP_MODULE(
     input uart_rx,
     //output uart_tx,
     
-    output [15:0] leds,
-    input [0:0] sw,
+    //output [15:0] leds,
+    //input [0:0] sw,
     
     inout sda,
-    inout scl   
+    inout scl
     );
-        
-     
-    //Defincion cables
+    
     //Variables UART
     
     wire [7:0] rx_data;
     wire rx_ready;
     //reg [7:0] tx_data;
     //reg tx_start;
-    //wire tx_busy;    
-    
-    
+    //wire tx_busy;
+
     //Instanciacion_modulos
     UART_RX_MASTER instance_uart_rx (
         .clk(clk_sys),
@@ -63,13 +60,14 @@ module TOP_MODULE(
         .tx_busy(tx_busy)
     );
     */
-    
+
     reg i2c_ena;
     reg [6:0] i2c_addr;
     reg i2c_rw;
     reg [7:0] i2c_data_wr;
     wire i2c_busy;
     wire [7:0] i2c_data_rd;
+    wire i2c_ack_error;
     
     i2c_master instance_i2c_master (
         .clk(clk_sys),
@@ -82,118 +80,233 @@ module TOP_MODULE(
         .data_rd(i2c_data_rd), //out 8
         .sda(sda),
         .scl(scl),
-        .ack_error()
+        .ack_error(i2c_ack_error)
     );
     
+    wire clk_1hz;
+    reg clk_1hz_prev;
+    Clock_Divisor #(1) instance_clk1hz(
+        .CLK100MHZ(clk_sys),
+        .clk(clk_1hz)
+    );
+    
+    //estados para recibir comando
+    reg [7:0] data_wr_1, data_wr_1_nxt;
+    reg [7:0] data_wr_2, data_wr_2_nxt;//seguir hasta 6 si se quiere escribir mas de 2 comandos
+    reg [2:0] largo_com, largo_com_nxt;
+    reg rw_1, rw_1_nxt;
+    reg rw_2, rw_2_nxt;
+    reg rw_3, rw_3_nxt;
+    reg rw_4, rw_4_nxt;
+    reg rw_5, rw_5_nxt;
+    reg rw_6, rw_6_nxt;
+    reg [6:0] addr=7'h00, addr_nxt;
+    always_comb begin
+        addr_nxt = addr;
+        data_wr_1_nxt = data_wr_1;
+        data_wr_2_nxt = data_wr_2;
+        rw_1_nxt = rw_1;
+        rw_2_nxt = rw_2;
+        rw_3_nxt = rw_3;
+        rw_4_nxt = rw_4;
+        rw_5_nxt = rw_5;
+        rw_6_nxt = rw_6;
+        
+        largo_com_nxt = largo_com;
+        if (rx_ready)
+            begin
+            case(rx_data)
+                8'd1://set up mpu
+                    begin
+                        addr_nxt = 7'h69;
+                        rw_1_nxt = 1'b0;
+                        rw_2_nxt = 1'b0;
+                        data_wr_1_nxt = 8'h6b;
+                        data_wr_2_nxt = 8'h00;
+                        largo_com_nxt = 3'd2;
+                    end
+                8'd2://pedir 6 registros aceleracion acelerometro
+                    begin
+                        addr_nxt = 7'h69;
+                        rw_1_nxt = 1'b0;
+                        rw_2_nxt = 1'b1;
+                        rw_3_nxt = 1'b1;
+                        rw_4_nxt = 1'b1;
+                        rw_5_nxt = 1'b1;
+                        rw_6_nxt = 1'b1;
+                        data_wr_1_nxt = 8'h3b;//pedir aceleraciones
+                        largo_com_nxt = 3'd6;
+                    end
+                8'd3://pedir 6 registros giro acelerometro
+                    begin
+                        addr_nxt = 7'h69;
+                        rw_1_nxt = 1'b0;
+                        rw_2_nxt = 1'b1;
+                        rw_3_nxt = 1'b1;
+                        rw_4_nxt = 1'b1;
+                        rw_5_nxt = 1'b1;
+                        rw_6_nxt = 1'b1;
+                        data_wr_1_nxt = 8'h44;//pedir giros
+                        largo_com_nxt = 3'd6;                        
+                    end
+                8'd4://pedir 6 registros hora
+                    begin
+                        addr_nxt = 7'h68;
+                        rw_1_nxt = 1'b0;
+                        rw_2_nxt = 1'b1;
+                        rw_3_nxt = 1'b1;
+                        rw_4_nxt = 1'b1;
+                        rw_5_nxt = 1'b1;
+                        rw_6_nxt = 1'b1;
+                        data_wr_1_nxt = 8'h00;//pedir seg-min-hora-dia-semana-mes
+                        largo_com_nxt = 3'd6;
+                    end
+            endcase
+            end
+    end
+    
+    always_ff @(posedge clk_sys) begin
+        addr <= addr_nxt;
+        rw_1 <= rw_1_nxt;
+        rw_2 <= rw_2_nxt;
+        rw_3 <= rw_3_nxt;
+        rw_4 <= rw_4_nxt;
+        rw_5 <= rw_5_nxt;
+        rw_6 <= rw_6_nxt;
+        data_wr_1 <= data_wr_1_nxt;
+        data_wr_2 <= data_wr_2_nxt;
+        largo_com <= largo_com_nxt;
+    end
+    //estado para i2c
     reg i2c_busy_prev;
-    reg [2:0] i2c_busy_cnt, i2c_busy_cnt_nxt; //max 7
-    reg [47:0] i2c_data_rd_last, i2c_data_rd_last_nxt; //almacena 6 datos
+    reg [2:0] i2c_busy_cnt, i2c_busy_cnt_nxt;
     reg i2c_ena_start, i2c_ena_start_nxt;
-    reg i2c_mpu_setup, i2c_mpu_setup2, i2c_mpu_setup_nxt;
+    reg i2c_rw_1;
+    reg i2c_rw_2;
+    reg i2c_rw_3;
+    reg i2c_rw_4;
+    reg i2c_rw_5;
+    reg i2c_rw_6;
+    reg [7:0] i2c_data_wr_1;
+    reg [7:0] i2c_data_wr_2;
+    reg [2:0] i2c_largo_com;
+    reg [6:0] i2c_addr_new;
     always_comb begin
         i2c_ena = 1'b0;
-        i2c_addr = 7'h00;
+        //i2c_addr = 7'h00;
         i2c_rw = 1'b0;
-        i2c_data_wr = 8'haa;
-        i2c_busy_cnt_nxt = i2c_busy_cnt;
-        i2c_data_rd_last_nxt = i2c_data_rd_last;
-        i2c_ena_start_nxt = (rx_ready==1'b1) ? 1'b1 : (i2c_busy==1'b1) ? 1'b0 : i2c_ena_start;
-        i2c_mpu_setup_nxt = i2c_mpu_setup;
-        
-        if ((i2c_busy_prev == 1'b0) && (i2c_busy == 1'b1)) i2c_busy_cnt_nxt = i2c_busy_cnt + 3'd1;
-        case(i2c_busy_cnt)
-            3'd0: 
+        i2c_data_wr = 8'h00;
+        i2c_busy_cnt_nxt = ((i2c_busy_prev == 1'b0) && (i2c_busy == 1'b1)) ? i2c_busy_cnt + 3'd1 : i2c_busy_cnt;
+        i2c_ena_start_nxt = ((clk_1hz_prev == 1'b0) && (clk_1hz == 1'b1)) ? 1'b1 : (i2c_busy == 1'b1) ? 1'b0 : i2c_ena_start;
+        case (i2c_busy_cnt)
+            3'd0:
                 begin
-                    if(i2c_mpu_setup) begin
-                        i2c_ena = sw;
-                        i2c_addr = 7'h69;
-                        i2c_rw = 1'b0;
-                        i2c_data_wr = 8'h6b;
-                    end else begin
-                        i2c_ena = i2c_ena_start;
-                        i2c_addr = 7'h69;
-                        i2c_rw = 1'b0;
-                        i2c_data_wr = 8'h3b;
-                    end
+                    i2c_ena = i2c_ena_start;
+                    //i2c_addr = i2c_addr_new;
+                    i2c_rw = i2c_rw_1;
+                    i2c_data_wr = i2c_data_wr_1;
                 end
-            3'd1://pido dato 1
+            3'd1:
                 begin
-                    if(i2c_mpu_setup) begin
-                        i2c_ena = 1'b1;
-                        i2c_addr = 7'h69;
-                        i2c_rw = 1'b0;
-                        i2c_data_wr = 8'h00;
-                    end else begin
-                        i2c_ena = 1'b1;
-                        i2c_addr = 7'h69;
-                        i2c_rw = 1'b1;
-                    end                                        
-                end
-            3'd2://pido dato 2
-                begin
-                    if(i2c_mpu_setup) begin
+                    if (i2c_largo_com == i2c_busy_cnt) begin
                         i2c_ena = 1'b0;
-                        if (i2c_busy == 1'b0) begin
-                            i2c_busy_cnt_nxt = 3'd0;
-                            i2c_mpu_setup_nxt = 1'b0;
-                        end
+                        if (i2c_busy == 1'b0) i2c_busy_cnt_nxt = 3'd0;
                     end else begin
-                        i2c_data_rd_last_nxt = {i2c_data_rd_last[47:8], i2c_data_rd};
                         i2c_ena = 1'b1;
-                        i2c_addr = 7'h69;
+                        //i2c_addr = 7'h69;
+                        i2c_rw = i2c_rw_2;
+                        i2c_data_wr = i2c_data_wr_2;
+                    end                
+                end
+            3'd2:
+                begin
+                    if (i2c_largo_com == i2c_busy_cnt) begin
+                        i2c_ena = 1'b0;
+                        if (i2c_busy == 1'b0) i2c_busy_cnt_nxt = 3'd0;
+                    end else begin
+                        i2c_ena = 1'b1;
+                        //i2c_addr = 7'h69;
+                        i2c_rw = i2c_rw_3;
+                        i2c_data_wr = i2c_data_wr_2;
+                    end                
+                end
+            3'd3:
+                begin
+                    if (i2c_largo_com == i2c_busy_cnt) begin
+                        i2c_ena = 1'b0;
+                        if (i2c_busy == 1'b0) i2c_busy_cnt_nxt = 3'd0;
+                    end else begin                
+                        i2c_ena = 1'b1;
+                        //i2c_addr = 7'h69;
+                        i2c_rw = i2c_rw_4;
+                        i2c_data_wr = 8'h04;
+                    end                
+                end
+            3'd4:
+                begin
+                    if (i2c_largo_com == i2c_busy_cnt) begin
+                        i2c_ena = 1'b0;
+                        if (i2c_busy == 1'b0) i2c_busy_cnt_nxt = 3'd0;
+                    end else begin
+                        i2c_ena = 1'b1;
+                        //i2c_addr = 7'h69;
+                        i2c_rw = i2c_rw_5;
+                        i2c_data_wr = 8'h05;
+                    end                
+                end
+            3'd5:
+                begin
+                    if (i2c_largo_com == i2c_busy_cnt) begin
+                        i2c_ena = 1'b0;
+                        if (i2c_busy == 1'b0) i2c_busy_cnt_nxt = 3'd0;
+                    end else begin
+                        i2c_ena = 1'b1;
+                        //i2c_addr = 7'h69;
+                        i2c_rw = i2c_rw_6;
+                        i2c_data_wr = 8'h06;
+                    end                
+                end
+            3'd6:
+                begin
+                    if (i2c_largo_com == i2c_busy_cnt) begin
+                        i2c_ena = 1'b0;
+                        if (i2c_busy == 1'b0) i2c_busy_cnt_nxt = 3'd0;
+                    end else begin
+                        i2c_ena = 1'b1;
+                        //i2c_addr = 7'h69;
                         i2c_rw = 1'b1;
+                        i2c_data_wr = 8'h07;
                     end
                 end
-            3'd3://pido dato 3
+            3'd7:
                 begin
-                    i2c_data_rd_last_nxt = {i2c_data_rd_last[47:16],i2c_data_rd , i2c_data_rd_last[7:0]};
-                    i2c_ena = 1'b1;
-                    i2c_addr = 7'h69;
-                    i2c_rw = 1'b1;
+                i2c_ena = 1'b0;
+                if (i2c_busy == 1'b0) i2c_busy_cnt_nxt = 3'd0;
                 end
-            3'd4://pido dato 4
-                begin
-                    i2c_data_rd_last_nxt = {i2c_data_rd_last[47:24],i2c_data_rd , i2c_data_rd_last[15:0]};
-                    i2c_ena = 1'b1;
-                    i2c_addr = 7'h69;
-                    i2c_rw = 1'b1;
-                end
-            3'd5://pido dato 5
-                begin
-                    i2c_data_rd_last_nxt = {i2c_data_rd_last[47:32],i2c_data_rd , i2c_data_rd_last[23:0]};
-                    i2c_ena = 1'b1;
-                    i2c_addr = 7'h69;
-                    i2c_rw = 1'b1;
-                end
-            3'd6://pido dato 6
-                begin
-                    i2c_data_rd_last_nxt = {i2c_data_rd_last[47:40],i2c_data_rd , i2c_data_rd_last[31:0]};
-                    i2c_ena = 1'b1;
-                    i2c_addr = 7'h69;
-                    i2c_rw = 1'b1;
-                end            
-            3'd7://termino la comunicacion
-                begin
-                    i2c_data_rd_last_nxt = {i2c_data_rd , i2c_data_rd_last[39:0]};
-                    i2c_ena = 1'b0;
-                    if (i2c_busy == 1'b0 ) i2c_busy_cnt_nxt = 3'd0;
-                end
-        endcase
+        endcase   
+            
     end
     
-    always_ff @ (posedge clk_sys) begin
-        if(~cpu_resetn) begin
-            i2c_busy_cnt <= 3'd0;
-            i2c_mpu_setup <= 1'd1;
-        end else begin 
-            i2c_busy_cnt  <= i2c_busy_cnt_nxt;
-            i2c_mpu_setup <= i2c_mpu_setup_nxt;
-        end
-        i2c_data_rd_last <= i2c_data_rd_last_nxt;
+    always_ff @(posedge clk_sys) begin
+        i2c_busy_cnt <= i2c_busy_cnt_nxt;
         i2c_ena_start <= i2c_ena_start_nxt;
         i2c_busy_prev <= i2c_busy;
-        i2c_mpu_setup2 <= i2c_mpu_setup;
+        
+        clk_1hz_prev <= clk_1hz;
+        
+        if (i2c_busy_cnt == 3'd0) begin
+            i2c_data_wr_1 <= data_wr_1;
+            i2c_data_wr_2 <= data_wr_2;
+            i2c_rw_1 = rw_1;
+            i2c_rw_2 = rw_2;
+            i2c_rw_3 = rw_3;
+            i2c_rw_4 = rw_4;
+            i2c_rw_5 = rw_5;
+            i2c_rw_6 = rw_6;
+            i2c_largo_com <= largo_com;
+            i2c_addr <= addr;
+        end
     end
     
-    assign leds = i2c_data_rd_last;
+    
 endmodule
